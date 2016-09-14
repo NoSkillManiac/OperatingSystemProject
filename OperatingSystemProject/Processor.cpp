9fe1cb14745020a6c155a09d7a3cfe2c0c64c7a6
TF2274 <clayton30022@hotmail.com>
2016-09-14 10:41:51 -0400

    Added Processor class. Implemented part of it.

--- OperatingSystemProject/Processor.cpp
+++ OperatingSystemProject/Processor.cpp
@@ -3,9 +3,148 @@
 
 Processor::Processor()
 {
+	this->registers = new int[16];
 }
-
 
 Processor::~Processor()
 {
+	delete this->registers;
 }
+
+void Processor::RD() {}; //not sure how to implement yet
+
+void Processor::WR() {}; //not sure how to implement yet
+
+void Processor::ST(unsigned char reg1, unsigned short addr)
+{
+	//not ready to implement
+};
+
+void Processor::LW(unsigned char reg1, unsigned short addr)
+{
+	//not ready to implement
+};
+
+void Processor::MOV(unsigned char reg1, unsigned char reg2)
+{
+	this->registers[reg2] = this->registers[reg1];
+};
+
+void Processor::ADD(unsigned char sreg1, unsigned char sreg2, unsigned char dreg)
+{
+	this->registers[dreg] = this->registers[sreg1] + this->registers[sreg2];
+};
+
+void Processor::SUB(unsigned char sreg1, unsigned char sreg2, unsigned char dreg)
+{
+	this->registers[dreg] = this->registers[sreg1] - this->registers[sreg2];
+};
+
+void Processor::MUL(unsigned char sreg1, unsigned char sreg2, unsigned char dreg)
+{
+	this->registers[dreg] = this->registers[sreg1] * this->registers[sreg2];
+};
+
+void Processor::DIV(unsigned char sreg1, unsigned char sreg2, unsigned char dreg)
+{
+	this->registers[dreg] = this->registers[sreg1] / this->registers[sreg2];
+};
+
+void Processor::AND(unsigned char sreg1, unsigned char sreg2, unsigned char dreg)
+{
+	this->registers[dreg] = this->registers[sreg1] && this->registers[sreg2];
+};
+
+void Processor::OR(unsigned char sreg1, unsigned char sreg2, unsigned char dreg)
+{
+	this->registers[dreg] = this->registers[sreg1] || this->registers[sreg2];
+};
+
+void Processor::MOVI(short data, unsigned char dreg)
+{
+	//not ready to implement
+};
+
+void Processor::ADDI(short imm, unsigned char dreg)
+{
+	//not ready to implement
+};
+
+void Processor::MULI(short imm, unsigned char dreg)
+{
+	//not ready to implement
+};
+
+void Processor::DIVI(short data, unsigned char dreg)
+{
+	//not ready to implement
+};
+
+void Processor::LDI(short data, unsigned char dreg)
+{
+	//not ready to implement
+};
+
+void Processor::SLT(unsigned char sreg, unsigned char breg, unsigned char dreg)
+{
+	if (this->registers[sreg] < this->registers[breg])
+		this->registers[dreg] = 1;
+	else
+		this->registers[dreg] = 0;
+};
+
+void Processor::SLTI(unsigned char sreg, short imm, unsigned char dreg)
+{
+	//not ready to implement
+};
+
+void Processor::HLT()
+{
+	//not ready to implement
+};
+
+void Processor::NOP()
+{
+	//no-op, this function intentionally left blank
+};
+
+void Processor::JMP(unsigned short addr)
+{
+	this->pc = addr;
+}
+
+void Processor::BEQ(unsigned char breg, unsigned char dreg, unsigned short addr)
+{
+	if (this->registers[breg] == this->registers[dreg])
+		this->pc = addr;
+};
+
+void Processor::BNE(unsigned char breg, unsigned char dreg, unsigned short addr)
+{
+	if (this->registers[breg] != this->registers[dreg])
+		this->pc = addr;
+};
+
+void Processor::BEZ(unsigned char breg, unsigned short addr)
+{
+	if (this->registers[breg] == 0)
+		this->pc = addr;
+};
+
+void Processor::BNZ(unsigned char breg, unsigned short addr)
+{
+	if (this->registers[breg] != 0)
+		this->pc = addr;
+};
+
+void Processor::BGZ(unsigned char breg, unsigned short addr)
+{
+	if (this->registers[breg] > 0)
+		this->pc = addr;
+};
+
+void Processor::BLZ(unsigned char breg, unsigned short addr)
+{
+	if (this->registers[breg] < 0)
+		this->pc = addr;
+};
--- OperatingSystemProject/Processor.h
+++ OperatingSystemProject/Processor.h
@@ -4,5 +4,40 @@ class Processor
 public:
 	Processor();
 	~Processor();
+	
+private:
+	//registers[0] is the accumulator
+	//registers[1] is the zero register and cannot be changed
+	//registers[2] --> registers[15] are general purpose registers
+	int* registers; //holds 16 integers, initialized in the constructor
+	unsigned short pc; //program counter
+
+	void RD(); //not sure how to implement yet
+	void WR(); //not sure how to implement yet
+	void ST(unsigned char reg1, unsigned short addr); //store contents of register into address
+	void LW(unsigned char reg1, unsigned short addr); //load contents of address into register
+	void MOV(unsigned char reg1, unsigned char reg2); //move contents from reg1 to reg2
+	void ADD(unsigned char sreg1, unsigned char sreg2, unsigned char dreg); //add content of both sregs to dreg
+	void SUB(unsigned char sreg1, unsigned char sreg2, unsigned char dreg); //sub sreg2 from sreg1 and store in dreg
+	void MUL(unsigned char sreg1, unsigned char sreg2, unsigned char dreg); //multiplies sregs and store in dreg
+	void DIV(unsigned char sreg1, unsigned char sreg2, unsigned char dreg); //sreg1/sreg2 --> dreg
+	void AND(unsigned char sreg1, unsigned char sreg2, unsigned char dreg); //logical AND of sreg1 and sreg2, store in dreg
+	void OR(unsigned char sreg1, unsigned char sreg2, unsigned char dreg); //logical OR of sreg1 and sreg2, store in dreg
+	void MOVI(short data, unsigned char dreg); //transfer data/addr into dreg
+	void ADDI(short imm, unsigned char dreg); //add imm to dreg
+	void MULI(short imm, unsigned char dreg); //multiply imm data by dreg data
+	void DIVI(short imm, unsigned char dreg); //divide dreg data by imm data
+	void LDI(short data, unsigned char dreg); //load data/address into dreg
+	void SLT(unsigned char sreg, unsigned char breg, unsigned char dreg); //set dreg to 1 if sreg < breg, 0 if otherwise
+	void SLTI(unsigned char sreg, short imm, unsigned char dreg); //set dreg to 1 if sreg < imm, 0 if otherwise
+	void HLT(); //logical end of the program
+	void NOP(); //no-op
+	void JMP(unsigned short addr); //jump to the set address (set the pc to the address of the instruction to jump to)
+	void BEQ(unsigned char breg, unsigned char dreg, unsigned short addr); //set pc to addr if breg == dreg
+	void BNE(unsigned char breg, unsigned char dreg, unsigned short addr); //set pc to addr if breg != dreg
+	void BEZ(unsigned char breg, unsigned short addr); //set pc to addr if breg == 0
+	void BNZ(unsigned char breg, unsigned short addr); //set pc to addr if breg != 0
+	void BGZ(unsigned char breg, unsigned short addr); //set pc to addr if breg > 0
+	void BLZ(unsigned char breg, unsigned short addr); //set pc to addr if breg < 0
 };
 
